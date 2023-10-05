b0361a72b95304cf6e57b81117ddc07581c4f6bf
Yadisa Belliard <93412709+YadiBelly@users.noreply.github.com>
2023-10-05 17:06:10 -0400

    card

--- src/pages/programs.tsx
+++ src/pages/programs.tsx
@@ -1,3 +1,111 @@
 export default function Programs() {
-  return <></>;
+  return (
+    <main className="min-h-screen">
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl text-white">Join our Programs</h1>
+        <div className="flex items-center space-x-2">
+          <button
+            type="button"
+            className="mb-2 mr-2 rounded-lg bg-primary_yellow px-5 py-2.5 text-sm font-medium text-black hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 dark:focus:ring-yellow-900"
+          >
+            Add Program
+          </button>
+          <Search />
+          <button
+            type="button"
+            className="mb-2 mr-2 rounded-lg bg-primary_yellow px-5 py-2.5 text-sm font-medium text-black hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 dark:focus:ring-yellow-900"
+          >
+            Filter
+          </button>
+        </div>
+      </div>
+      {/* Cards */}
+      <div className="max-w-sm rounded-lg border border-gray-200 bg-white shadow dark:border-gray-700 dark:bg-gray-800">
+        <a href="#">
+          <img
+            className="rounded-t-lg"
+            src="/docs/images/blog/image-1.jpg"
+            alt=""
+          />
+        </a>
+        <div className="p-5">
+          <a href="#">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Noteworthy technology acquisitions 2021
+            </h5>
+          </a>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+          <a
+            href="#"
+            className="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Read more
+            <svg
+              className="ml-2 h-3.5 w-3.5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </a>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+function Search() {
+  return (
+    <form>
+      <label
+        htmlFor="default-search"
+        className="sr-only mb-2 text-sm font-medium text-gray-900"
+      >
+        Search
+      </label>
+      <div className="relative">
+        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+          <svg
+            className="h-4 w-4 text-gray-500"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
+        </div>
+        <input
+          type="search"
+          id="default-search"
+          className="block w-full rounded-md border border-gray-300 bg-white p-2 pl-10 text-sm text-gray-900 focus:border-yellow-500 focus:ring-yellow-500"
+          placeholder="Search for programs..."
+          required
+        />
+        <button
+          type="submit"
+          className="absolute inset-y-0 right-0 flex items-center justify-center rounded-r-lg bg-primary_yellow px-4 py-2 text-sm font-medium text-black hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300"
+        >
+          Search
+        </button>
+      </div>
+    </form>
+  );
 }
