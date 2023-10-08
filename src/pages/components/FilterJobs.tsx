d141c3e8b0622c826e7ee1c52467fcd4858dbb3e
Ale <alejandrocartagena68@gmail.com>
2023-10-08 01:41:33 -0400

    Fixed the padding on the checkboxes

--- src/pages/components/FilterJobs.tsx
+++ src/pages/components/FilterJobs.tsx
@@ -9,31 +9,31 @@ export default function() {
           <div className="mb-2">
             <h3 className="text-base mb-2">Job Type</h3>
             {/* Full time box */}
-            <div className="mb-1.5">
+            <div className="mb-1.5 flex">
               <input 
               type="checkbox" 
               name="fulltime" 
               id="fulltime"
               className="mr-1.5 rounded-sm cursor-pointer" />
-              <label className="text-sm font-normal cursor-pointer" htmlFor="fulltime">Fulltime</label>
+              <label className="text-sm font-normal cursor-pointer pt-[0.1rem]" htmlFor="fulltime">Fulltime</label>
             </div>
             {/* Internship box */}
-            <div className="mb-1.5">
+            <div className="mb-1.5 flex">
               <input 
               type="checkbox" 
               name="internship" 
               id="internship"
               className="mr-1.5 rounded-sm cursor-pointer" />
-              <label className="text-sm font-normal cursor-pointer" htmlFor="internship">Internship</label>
+              <label className="text-sm font-normal cursor-pointer pt-[0.1rem]" htmlFor="internship">Internship</label>
             </div>
             {/* New-grad box */}
-            <div className="mb-1.5">
+            <div className="mb-1.5 flex">
               <input 
               type="checkbox" 
               name="new-grad" 
               id="new-grad"
               className="mr-1.5 rounded-sm cursor-pointer" />
-              <label className="text-sm font-normal cursor-pointer" htmlFor="new-grad">New-grad</label>
+              <label className="text-sm font-normal cursor-pointer pt-[0.1rem]" htmlFor="new-grad">New-grad</label>
             </div>
           </div>
 
@@ -41,31 +41,31 @@ export default function() {
           <div className="mb-9">
             <h3 className="text-base mb-2">Job location</h3>
             {/* On-site box */}
-            <div className="mb-1.5">
+            <div className="mb-1.5 flex">
               <input 
               type="checkbox" 
               name="on-site" 
               id="on-site"
               className="mr-1.5 rounded-sm cursor-pointer" />
-              <label className="text-sm font-normal cursor-pointer" htmlFor="on-site">On-site</label>
+              <label className="text-sm font-normal cursor-pointer pt-[0.1rem]" htmlFor="on-site">On-site</label>
             </div>
             {/* Hybrid box */}
-            <div className="mb-1.5">
+            <div className="mb-1.5 flex">
               <input 
               type="checkbox" 
               name="hybrid" 
               id="hybrid" 
               className="mr-1.5 rounded-sm cursor-pointer"/>
-              <label className="text-sm font-normal cursor-pointer" htmlFor="hybrid">Hybrid</label>
+              <label className="text-sm font-normal cursor-pointer pt-[0.1rem]" htmlFor="hybrid">Hybrid</label>
             </div>
             {/* New-grad box */}
-            <div className="mb-1.5">
+            <div className="mb-1.5 flex">
               <input 
               type="checkbox" 
               name="remote" 
               id="remote"
               className="mr-1.5 rounded-sm cursor-pointer" />
-              <label className="text-sm font-normal cursor-pointer" htmlFor="remote">Remote</label>
+              <label className="text-sm font-normal cursor-pointer pt-[0.1rem]" htmlFor="remote">Remote</label>
             </div>
 
           </div>
