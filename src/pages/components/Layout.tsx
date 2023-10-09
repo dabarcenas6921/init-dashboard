c7dffd5a694ccd6b3ace9ee29de72f4786598525
Yadisa Belliard <93412709+YadiBelly@users.noreply.github.com>
2023-10-09 17:06:25 -0400

    took off search function

--- src/pages/components/Layout.tsx
+++ src/pages/components/Layout.tsx
@@ -2,10 +2,10 @@ import React, { PropsWithChildren } from "react";
 import Navbar from "./Navbar";
 const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <>
+    <div className="max-w-screen-2xl p-8">
       <Navbar />
       {children}
-    </>
+    </div>
   );
 };
 export default Layout;
--- src/pages/programs.tsx
+++ src/pages/programs.tsx
@@ -3,21 +3,7 @@ export default function Programs() {
     <main className="min-h-screen">
       <div className="flex items-center justify-between">
         <h1 className="text-3xl text-white">Programs</h1>
-        <div className="flex items-center space-x-5">
-          <button
-            type="button"
-            className="rounded-lg bg-primary_yellow px-5 py-2.5 text-sm font-medium text-black hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300"
-          >
-            Add Program
-          </button>
-          <Search />
-          <button
-            type="button"
-            className="rounded-lg bg-primary_yellow px-5 py-2.5 text-sm font-medium text-black hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300"
-          >
-            Filter
-          </button>
-        </div>
+        <div className="flex items-center space-x-5"></div>
       </div>
       {/* Card */}
       <div className="dark:border-none-700 max-w-sm rounded-lg border border-gray-200 bg-white shadow dark:bg-gray-800">
@@ -66,50 +52,5 @@ export default function Programs() {
         </div>
       </div>
     </main>
-  );
-}
-
-function Search() {
-  return (
-    <form className="flex items-center">
-      <label
-        htmlFor="default-search"
-        className="sr-only text-sm font-medium text-gray-900"
-      >
-        Program
-      </label>
-      <div className="relative">
-        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-          <svg
-            className="h-4 w-4 text-gray-500"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 20"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-            />
-          </svg>
-        </div>
-        <input
-          type="search"
-          id="default-search"
-          className="border-yellow_primary block w-full rounded-md border bg-white p-2 pl-10 text-sm text-gray-900 focus:border-yellow-500 focus:ring-yellow-500"
-          placeholder="Search for programs..."
-          required
-        />
-        <button
-          type="submit"
-          className="absolute inset-y-0 right-0 flex items-center justify-center rounded-r-lg bg-primary_yellow px-4 py-2 text-sm font-medium text-black hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300"
-        >
-          Programs
-        </button>
-      </div>
-    </form>
   );
 }
