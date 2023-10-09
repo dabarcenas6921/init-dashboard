026ca1c91bbf693203f23f908512fc36ced8d0c8
David Barcenas <dabarcenas6921@gmail.com>
2023-10-09 16:56:11 -0400

    fixed broken api call

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,6 @@
 import Head from "next/head";
-import { api } from "~/utils/api";
-import Navbar from "./components/Navbar";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   return (
     <>
       <Head>
