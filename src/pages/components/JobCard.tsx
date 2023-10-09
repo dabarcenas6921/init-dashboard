1b4bace6da32d35e528187b7501df82c548e4e82
Ale <alejandrocartagena68@gmail.com>
2023-10-09 17:11:31 -0400

    Working on job card component

---
+++ src/pages/components/JobCard.tsx
@@ -0,0 +1,48 @@
+export default function JobCard() {
+    return(
+        <div className="w-full max-w-[20rem] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div className="flex justify-end px-4 pt-4">
+                <button id="dropdownButton" data-dropdown-toggle="dropdown" className="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5" type="button">
+                    <span className="sr-only">Open dropdown</span>
+                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
+                        <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"/>
+                    </svg>
+                </button>
+                {/* Dropdown menu */}
+                <div id="dropdown" className="z-10 hidden text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                    <ul className="py-2" aria-labelledby="dropdownButton">
+                        <li>
+                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Edit</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Export Data</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            {/* Card Contents */}
+            <div className="flex flex-col items-center pb-10">
+                {/* Image, Job Title, and Company Name */}
+                <div>
+                    <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image"/>
+                    <h3></h3>
+                    <p></p>
+                </div>
+                {/* Pill Badges for Job Tags (Fulltime, Internship, Remote, etc) */}
+                <div>
+                    <span className="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300">Default</span>
+                    <span className="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">Green</span>
+                    <span className="bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-yellow-900 dark:text-yellow-300">Yellow</span>
+                </div>
+                {/* Date Posted and Apply Button link */}
+                <div className="flex mt-4 space-x-3 md:mt-6">
+                    <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Apply</a>
+                </div>
+            </div>
+        </div>
+
+    );
+}
--- src/pages/jobs.tsx
+++ src/pages/jobs.tsx
@@ -1,4 +1,5 @@
 import FilterJobs from './components/FilterJobs';
+import JobCard from './components/JobCard';
 
 export default function Jobs() {
   return (
@@ -7,7 +8,21 @@ export default function Jobs() {
       {/* Container to keep everything in line */}
       <div className="p-4 max-w-screen-xl mx-auto">
         <h1 className="text-3xl mb-8">Job Board</h1>
-        <FilterJobs></FilterJobs>
+        <div className="flex">
+
+          {/* Filter Jobs container */}
+          <div className="mr-4 md:mr-24">
+            <FilterJobs></FilterJobs>
+          </div>
+          
+          {/* Job Cards container */}
+          <div className="border-s-orange-600 border-2 w-full">
+            <JobCard></JobCard>
+          </div>
+          
+
+        </div>
+        
 
       </div>
       
@@ -17,3 +32,18 @@ export default function Jobs() {
     
   );
 }
+
+/*
+  
+jobPost = {
+	image: "img URL"
+	title: ""
+	company: ""
+	location: "On/site, Remote, Hybrid"
+	type: "internship, new grad"
+	hours: "Fulltime, part-time"
+	datePosted: ""
+	link: "job posting URL"
+}
+
+*/
