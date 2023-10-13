a14818984eeed98fcc1b8a328badc6dfffde25aa
Ale <alejandrocartagena68@gmail.com>
2023-10-13 01:39:45 -0400

    Started adding styles to the job cards and made them responsive

--- src/pages/components/JobCard.tsx
+++ src/pages/components/JobCard.tsx
@@ -5,7 +5,7 @@ import { Card, Dropdown } from 'flowbite-react';
 
 export default function UserProfileCard() {
   return (
-    <Card>
+    <Card className="max-w-xs min-w-[16rem] bg-[#1A1E22] flex-grow mr-4 max-[820px]:mb-8">
       <div className="text-black flex justify-end px-4 pt-4">
         <Dropdown
           inline
@@ -44,38 +44,16 @@ export default function UserProfileCard() {
           </Dropdown.Item>
         </Dropdown>
       </div>
-      <div className="flex flex-col items-center pb-10">
-        <img
-          alt="Bonnie image"
-          className="mb-3 rounded-full shadow-lg"
-          height="96"
-          src="/images/people/profile-picture-3.jpg"
-          width="96"
-        />
-        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-          Bonnie Green
-        </h5>
-        <span className="text-sm text-gray-500 dark:text-gray-400">
-          Visual Designer
-        </span>
-        <div className="mt-4 flex space-x-3 lg:mt-6">
-          <a
-            className="inline-flex items-center rounded-lg bg-cyan-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
-            href="#"
-          >
-            <p>
-              Add friend
-            </p>
-          </a>
-          <a
-            className="inline-flex items-center rounded-lg border border-gray-300 bg-white px-4 py-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-            href="#"
-          >
-            <p>
-              Message
-            </p>
-          </a>
+      {/* Top of Card */}
+      <div className="flex">
+        <div className="">
+          <img className="w-10" src="https://www.freepnglogos.com/uploads/netflix-logo-app-png-16.png"></img>
         </div>
+        <div>
+          <h3 className="text-white">Job Title</h3>
+          <p>Company Name</p>
+        </div>
+        
       </div>
     </Card>
   )
--- src/pages/jobs.tsx
+++ src/pages/jobs.tsx
@@ -7,19 +7,22 @@ export default function Jobs() {
 
       {/* Container to keep everything in line */}
       <div className="p-4 max-w-screen-xl mx-auto">
-        <h1 className="text-3xl mb-8">Job Board</h1>
+        <h1 className="text-3xl mb-8 text-center md:text-left">Job Board</h1>
 
         <div className="flex flex-col md:flex-row">
 
           {/* Filter Jobs container */}
-          <div className="max-w-screen-md mx-auto md:mr-[5%]">
+          <div className="max-w-screen-md mx-auto md:mr-[2%]">
             <div className="mr-4 ">
               <FilterJobsDropDown></FilterJobsDropDown>
             </div>
           </div>
           
           {/* Job Cards container */}
-          <div className="border-s-orange-600 border-2 w-full">
+          <div className="border-s-orange-600 border-2 w-full flex flex-wrap items-start justify-evenly pl-4">
+            <JobCard></JobCard>
+            <JobCard></JobCard>
+            <JobCard></JobCard>
             <JobCard></JobCard>
           </div>
           
