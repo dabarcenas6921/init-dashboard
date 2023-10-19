b4c9079c97d82ca01c22e6b89c6697ef5ffbde5a
Yadisa Belliard <93412709+YadiBelly@users.noreply.github.com>
2023-10-19 15:44:34 -0400

    card

--- src/pages/programs.tsx
+++ src/pages/programs.tsx
@@ -8,7 +8,7 @@ export default function Programs() {
 
       {/* Card for Init Build*/}
       <div className="mt-12">
-        <div className="dark:border-none-700 w-1/2 max-w-sm rounded-lg border border-gray-200 bg-white shadow dark:bg-gray-800 md:w-1/4">
+        <div className="dark:border-none-600 w-1/2 max-w-sm rounded-lg border border-gray-200 bg-white shadow dark:bg-gray-800 md:w-1/4">
           <a href="#">
             <img
               className="rounded-t-lg"
@@ -53,9 +53,55 @@ export default function Programs() {
           </div>
         </div>
       </div>
-      {/* Card for Init Reach*/}
-      <div className="mt-12">
-        <div className="dark:border-none-700 w-1/2 max-w-sm rounded-lg border border-gray-200 bg-white shadow dark:bg-gray-800 md:w-1/4">
+
+      {/* Card for Reach Build*/}
+      <div className="build mt-12">
+        <div className="dark:border-none-800 w-1/2 max-w-sm rounded-lg border border-gray-200 bg-white shadow dark:bg-gray-800 md:w-1/4">
+          <a href="#">
+            <img
+              className="rounded-t-lg"
+              src="https://s3-alpha-sig.figma.com/img/0ec1/da54/6cf38b347bee1dcc6b49c386b1768072?Expires=1698624000&Signature=ku4MuOGJU-HqRhaLZG~rQ~ZMWFYb8LiSZT42juYGag3E0Piyh1oc9YyjR2dOr6HDbqd5iLrGZtTPfLLwZdRWlI~moY2YC0n2pD4QrjtWYjp0osOQtFTgKuVMNFSoxNNMXYnb-VDB05vsZLfVZDrrRAm1LPst5suNPFOnJCJQiZ1JXVVCEpAhjyYUTrQ8X~Lb19evV0zTyNZbHnJZBxwyEgwEWevONS13a8A9qV-g9sj3j9NIzJKVjeiuOIhVCvncvaeJTZstOBnCFmcT14o9jeO7eX9m1FX1SJ1i28YN8geAHACH6yS8mKLwQbMJS1OPjpRb2bTZGtPXO5z5NiD9aw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+              alt=""
+            />
+          </a>
+          <div className="p-5">
+            <a href="#">
+              <h5 className="light:text-blue mb-2 text-2xl font-bold tracking-tight text-blue-500">
+                Init Reach
+              </h5>
+            </a>
+            <p className="text-none-700 dark:text-none-400 mb-3 font-normal">
+              INIT Reach is a program focused on the hardware development side
+              of technology. Using all sorts of machinery, large or small, this
+              program aims inspire members to create their own devices with the
+              technology available today.
+            </p>
+            <a
+              href="#"
+              className="inline-flex items-center rounded-lg bg-blue-500 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Read more
+              <svg
+                className="ml-2 h-3.5 w-3.5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="reach mt-12">
+        <div className="dark:border-none-700 flex-direction:row w-1/2 max-w-sm rounded-lg border border-gray-200 bg-white shadow dark:bg-gray-800 md:w-1/4">
           <a href="#">
             <img
               className="rounded-t-lg"
