7f5de55116926405617138baa7610fd155ae8e17
David Barcenas <dabarcenas6921@gmail.com>
2023-10-19 01:52:53 -0400

    fixed interface

--- src/pages/components/EventCard.tsx
+++ src/pages/components/EventCard.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import type { EventCard } from "../interfaces/EventCard.interface";
+import type { EventCard } from "../../interfaces/EventCard.interface";
 import initLogo from "../../assets/init.png";
 
 export default function EventCard({
