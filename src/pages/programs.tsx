4abdaf2a8fc01b11eb9acb724471174873d60564
Yadisa Belliard <93412709+YadiBelly@users.noreply.github.com>
2023-10-25 22:19:13 -0400

    mapping

--- src/pages/programs.tsx
+++ src/pages/programs.tsx
@@ -5,25 +5,86 @@
 export default function Programs() {
   const sampleData = [
     {
-      title: "init build",
+      // {index 1} index in mapping
+      title: "Init Explore",
       image:
-        "https://s3-alpha-sig.figma.com/img/bdfa/e996/ec49d92b4a38e9615eb9f39c6126b252?Expires=1698019200&Signature=flYOVQDABPxN8w9~PQPrGi147fl0lhnXkgsf0l1hsC~eQ4myKRWMZjchmZeOQs2reqk4uEvGg0o8AgVVAKl~snuJZA00u-DN6vRps0t4Cx4Itey-sL7vPmJZ26u58DoXo6D72kkmpNBDQX2HjOZSGsxBjtplytJTvsVLf9YsfI7Nk08aJH~v2Lg40m0-U5EtSzRWvJXYwQOpAYbOvyBhVg~c7c3rejMOUVAa7hTZ7ckU91h-QEKwEv0eZ5iBiv34XpcaKk-FVdVvbOyaR2Qv-mbFBoRMCBakk8CQMZPbSzVF7mr7LYq6NwwYM76eaYu2ZN-mYWIafS0PzwkQzwffoQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+        "https://s3-alpha-sig.figma.com/img/bdfa/e996/ec49d92b4a38e9615eb9f39c6126b252?Expires=1699228800&Signature=dftADo4bJNSyWDw8ZDgxKAgQmRS86965Ckp3piZEAAQTQdTiqizRrCtSIyOeCuL1jszTfA9qYNxyfc2mPZ2aCOtDVSSTid13ov6DCQu-x4EwSFmpkGucnkvdLOJM8cgpzHul3Mf4cLRGU2LWLfdmWGg6DyVG4e1YZUaBtfz3fsAtI4ocjbeHyVHs43gm4y61DiX~79T7iQpnSY~KZ0lBLQrRhqMqHJ13DibK-JEAIbRrUm2vVS~ljaVTQIUZ06T~-PJBQWrcm4E4yqc6RV-MPXXs6BTSguZRjhTW-eZ9Tz9NJnhX2ny89QA8HMPnnQ6yTKdlksoSFl6jRkaV5sQobw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
       discription:
         "INIT Explore is a program focused on the software development side of technology. From web development to machine learning to simple scripts, this program aims to expose members to all sorts of different programming languages and frameworks commonly used in the industry.",
+
+      open: true,
+      //pages that applications will be filled up
+      url: "https://generalassemb.ly/education/data-analytics-immersive",
     },
     {
+      // {index 2} index in mapping
       title: "init reach",
       image:
         "https://s3-alpha-sig.figma.com/img/0ec1/da54/6cf38b347bee1dcc6b49c386b1768072?Expires=1698624000&Signature=ku4MuOGJU-HqRhaLZG~rQ~ZMWFYb8LiSZT42juYGag3E0Piyh1oc9YyjR2dOr6HDbqd5iLrGZtTPfLLwZdRWlI~moY2YC0n2pD4QrjtWYjp0osOQtFTgKuVMNFSoxNNMXYnb-VDB05vsZLfVZDrrRAm1LPst5suNPFOnJCJQiZ1JXVVCEpAhjyYUTrQ8X~Lb19evV0zTyNZbHnJZBxwyEgwEWevONS13a8A9qV-g9sj3j9NIzJKVjeiuOIhVCvncvaeJTZstOBnCFmcT14o9jeO7eX9m1FX1SJ1i28YN8geAHACH6yS8mKLwQbMJS1OPjpRb2bTZGtPXO5z5NiD9aw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
       discription:
         "INIT Reach is a program focused on the hardware development side of technology. Using all sorts of machinery, large or small, this program aims inspire members to create their own devices with the technology available today.",
+      open: true,
+      url: "https://generalassemb.ly/education/software-engineering-immersive",
     },
     {
+      // {index 3} index in mapping
       title: "init explore",
       image:
-        "https://s3-alpha-sig.figma.com/img/bdfa/e996/ec49d92b4a38e9615eb9f39c6126b252?Expires=1698019200&Signature=flYOVQDABPxN8w9~PQPrGi147fl0lhnXkgsf0l1hsC~eQ4myKRWMZjchmZeOQs2reqk4uEvGg0o8AgVVAKl~snuJZA00u-DN6vRps0t4Cx4Itey-sL7vPmJZ26u58DoXo6D72kkmpNBDQX2HjOZSGsxBjtplytJTvsVLf9YsfI7Nk08aJH~v2Lg40m0-U5EtSzRWvJXYwQOpAYbOvyBhVg~c7c3rejMOUVAa7hTZ7ckU91h-QEKwEv0eZ5iBiv34XpcaKk-FVdVvbOyaR2Qv-mbFBoRMCBakk8CQMZPbSzVF7mr7LYq6NwwYM76eaYu2ZN-mYWIafS0PzwkQzwffoQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+        "https://s3-alpha-sig.figma.com/img/1768/8492/709c016b0d1b66b4d4022e8e00245f52?Expires=1699228800&Signature=GWQs6EoiUUDSDKKyA5TvOv67JQJVA-hz4Blb3oVRISYz-mYGUtgAn56nuhfq1KErhZncwX1rQ1xtXcWqaiV8PkL3OYtIxYvZ5MKtnBSa~9mJz23uNiU2xMpKdpyqJceInhJ~HKdIWDNriyoqh0U72zF3mb5Tpk4qOOVT5Vfv3ekl3GKuj72kfwapWp-F6ApHb38SaOpyiTHVyB7vXpkgpiIhCwla-MvOl8TRbS6kbh7vhd9sccye9v6rR~BVa8HlBaBaXaJu230URk42tf69t~Q~qjxSJXZrkwD~-1cILwZlBJrR3248gi6NgqAH7qN0-~X2I3z7Vz9PfsiyIb45Zg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
       discription:
-        "INIT Explore is a program focused on the software development side of technology. From web development to machine learning to simple scripts, this program aims to expose members to all sorts of different programming languages and frameworks commonly used in the industry.",
+        "INIT Build is a program focused on project development. Teams have nine weeks to complete a project corresponding to a topic in technology that they may be interested in. From mobile applications to games to even robots.",
+      open: false,
+      url: "https://generalassemb.ly/education/user-experience-design-immersive",
+    },
+    {
+      // {index 3} index in mapping
+      title: "init explore",
+      image:
+        "https://s3-alpha-sig.figma.com/img/1768/8492/709c016b0d1b66b4d4022e8e00245f52?Expires=1699228800&Signature=GWQs6EoiUUDSDKKyA5TvOv67JQJVA-hz4Blb3oVRISYz-mYGUtgAn56nuhfq1KErhZncwX1rQ1xtXcWqaiV8PkL3OYtIxYvZ5MKtnBSa~9mJz23uNiU2xMpKdpyqJceInhJ~HKdIWDNriyoqh0U72zF3mb5Tpk4qOOVT5Vfv3ekl3GKuj72kfwapWp-F6ApHb38SaOpyiTHVyB7vXpkgpiIhCwla-MvOl8TRbS6kbh7vhd9sccye9v6rR~BVa8HlBaBaXaJu230URk42tf69t~Q~qjxSJXZrkwD~-1cILwZlBJrR3248gi6NgqAH7qN0-~X2I3z7Vz9PfsiyIb45Zg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      discription:
+        "INIT Build is a program focused on project development. Teams have nine weeks to complete a project corresponding to a topic in technology that they may be interested in. From mobile applications to games to even robots.",
+      open: false,
+      url: "https://generalassemb.ly/education/user-experience-design-immersive",
+    },
+    {
+      // {index 3} index in mapping
+      title: "init explore",
+      image:
+        "https://s3-alpha-sig.figma.com/img/1768/8492/709c016b0d1b66b4d4022e8e00245f52?Expires=1699228800&Signature=GWQs6EoiUUDSDKKyA5TvOv67JQJVA-hz4Blb3oVRISYz-mYGUtgAn56nuhfq1KErhZncwX1rQ1xtXcWqaiV8PkL3OYtIxYvZ5MKtnBSa~9mJz23uNiU2xMpKdpyqJceInhJ~HKdIWDNriyoqh0U72zF3mb5Tpk4qOOVT5Vfv3ekl3GKuj72kfwapWp-F6ApHb38SaOpyiTHVyB7vXpkgpiIhCwla-MvOl8TRbS6kbh7vhd9sccye9v6rR~BVa8HlBaBaXaJu230URk42tf69t~Q~qjxSJXZrkwD~-1cILwZlBJrR3248gi6NgqAH7qN0-~X2I3z7Vz9PfsiyIb45Zg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      discription:
+        "INIT Build is a program focused on project development. Teams have nine weeks to complete a project corresponding to a topic in technology that they may be interested in. From mobile applications to games to even robots.",
+      open: false,
+      url: "https://generalassemb.ly/education/user-experience-design-immersive",
+    },
+    {
+      // {index 3} index in mapping
+      title: "init explore",
+      image:
+        "https://s3-alpha-sig.figma.com/img/1768/8492/709c016b0d1b66b4d4022e8e00245f52?Expires=1699228800&Signature=GWQs6EoiUUDSDKKyA5TvOv67JQJVA-hz4Blb3oVRISYz-mYGUtgAn56nuhfq1KErhZncwX1rQ1xtXcWqaiV8PkL3OYtIxYvZ5MKtnBSa~9mJz23uNiU2xMpKdpyqJceInhJ~HKdIWDNriyoqh0U72zF3mb5Tpk4qOOVT5Vfv3ekl3GKuj72kfwapWp-F6ApHb38SaOpyiTHVyB7vXpkgpiIhCwla-MvOl8TRbS6kbh7vhd9sccye9v6rR~BVa8HlBaBaXaJu230URk42tf69t~Q~qjxSJXZrkwD~-1cILwZlBJrR3248gi6NgqAH7qN0-~X2I3z7Vz9PfsiyIb45Zg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      discription:
+        "INIT Build is a program focused on project development. Teams have nine weeks to complete a project corresponding to a topic in technology that they may be interested in. From mobile applications to games to even robots.",
+      open: false,
+      url: "https://generalassemb.ly/education/user-experience-design-immersive",
+    },
+    {
+      // {index 3} index in mapping
+      title: "init explore",
+      image:
+        "https://s3-alpha-sig.figma.com/img/1768/8492/709c016b0d1b66b4d4022e8e00245f52?Expires=1699228800&Signature=GWQs6EoiUUDSDKKyA5TvOv67JQJVA-hz4Blb3oVRISYz-mYGUtgAn56nuhfq1KErhZncwX1rQ1xtXcWqaiV8PkL3OYtIxYvZ5MKtnBSa~9mJz23uNiU2xMpKdpyqJceInhJ~HKdIWDNriyoqh0U72zF3mb5Tpk4qOOVT5Vfv3ekl3GKuj72kfwapWp-F6ApHb38SaOpyiTHVyB7vXpkgpiIhCwla-MvOl8TRbS6kbh7vhd9sccye9v6rR~BVa8HlBaBaXaJu230URk42tf69t~Q~qjxSJXZrkwD~-1cILwZlBJrR3248gi6NgqAH7qN0-~X2I3z7Vz9PfsiyIb45Zg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      discription:
+        "INIT Build is a program focused on project development. Teams have nine weeks to complete a project corresponding to a topic in technology that they may be interested in. From mobile applications to games to even robots.",
+      open: false,
+      url: "https://generalassemb.ly/education/user-experience-design-immersive",
+    },
+    {
+      // {index 3} index in mapping
+      title: "init explore",
+      image:
+        "https://s3-alpha-sig.figma.com/img/1768/8492/709c016b0d1b66b4d4022e8e00245f52?Expires=1699228800&Signature=GWQs6EoiUUDSDKKyA5TvOv67JQJVA-hz4Blb3oVRISYz-mYGUtgAn56nuhfq1KErhZncwX1rQ1xtXcWqaiV8PkL3OYtIxYvZ5MKtnBSa~9mJz23uNiU2xMpKdpyqJceInhJ~HKdIWDNriyoqh0U72zF3mb5Tpk4qOOVT5Vfv3ekl3GKuj72kfwapWp-F6ApHb38SaOpyiTHVyB7vXpkgpiIhCwla-MvOl8TRbS6kbh7vhd9sccye9v6rR~BVa8HlBaBaXaJu230URk42tf69t~Q~qjxSJXZrkwD~-1cILwZlBJrR3248gi6NgqAH7qN0-~X2I3z7Vz9PfsiyIb45Zg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      discription:
+        "INIT Build is a program focused on project development. Teams have nine weeks to complete a project corresponding to a topic in technology that they may be interested in. From mobile applications to games to even robots.",
+      open: false,
+      url: "https://generalassemb.ly/education/user-experience-design-immersive",
     },
   ];
   return (
@@ -32,15 +93,16 @@ export default function Programs() {
         <h1 className="mt-6 text-3xl text-white">Programs</h1>
         <div className="flex items-center space-x-5"></div>
       </div>
-      <div className="Cards flex">
+      <div className="Cards grid grid-cols-4 gap-x-8 gap-y-4 space-x-6">
+        {/* what ever comes first in the array thats the objects in the array and whats second is the Index */}
         {sampleData.map((programData, index) => {
           return (
-            <div key={index} className="mt-12 flex flex-row space-x-4">
-              <div className="dark:border-none-600 w-full max-w-sm rounded-lg border border-gray-200 bg-white shadow dark:bg-gray-800 ">
+            <div key={index} className="mt-12 flex flex-row  space-x-10">
+              <div className="dark:border-none-600  w-full max-w-sm rounded-lg border border-gray-200 bg-white shadow dark:bg-gray-800  ">
                 <a href="#">
                   <img
                     className="rounded-t-lg"
-                    alt="programimg"
+                    alt=""
                     src={programData.image}
                   />
                 </a>
@@ -50,30 +112,65 @@ export default function Programs() {
                       {programData.title}
                     </h5>
                   </a>
-                  <p className="text-none-700 dark:text-none-400 mb-3 font-normal">
+                  <p className="text-none-700 dark:text-none-400 mb-3 font-poppins">
                     {programData.discription}
                   </p>
-                  <a
-                    href="#"
-                    className="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
-                  >
-                    Read more
-                    <svg
-                      className="ml-2 h-3.5 w-3.5"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 14 10"
-                    >
-                      <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M1 5h12m0 0L9 1m4 4L9 9"
-                      />
-                    </svg>
-                  </a>
+                  {/* (Ternary.Operator) anything before ? is the condition if the conditon is true it
+                  needs to show whats on the left : and if its false everything
+                  on thr right on the : exampleOne.true ? : exampleTwo.false */}
+                  {programData.open ? (
+                    <>
+                      <a
+                        // This helps Url open in a different tab
+                        target="_blank"
+                        href={programData.url}
+                        className="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+                      >
+                        Apply Now
+                        <svg
+                          className="ml-2 h-3.5 w-3.5"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 14 10"
+                        >
+                          <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M1 5h12m0 0L9 1m4 4L9 9"
+                          />
+                        </svg>
+                      </a>
+                    </>
+                  ) : (
+                    <>
+                      <a
+                        // This helps Url open in a different tab
+                        target="_blank"
+                        // href={programData.url}
+                        className=" inline-flex items-center rounded-lg bg-blue-300 px-3 py-2 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-red-300 dark:bg-gray-400"
+                      >
+                        Applications Closed
+                        <svg
+                          className="ml-2 h-3.5 w-3.5"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 14 10"
+                        >
+                          <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M1 5h12m0 0L9 1m4 4L9 9"
+                          />
+                        </svg>
+                      </a>
+                    </>
+                  )}
                 </div>
               </div>
             </div>
