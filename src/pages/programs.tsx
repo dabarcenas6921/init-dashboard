05458734c28e99057739b7a72b2aa08f155a50ab
Yadisa Belliard <93412709+YadiBelly@users.noreply.github.com>
2023-10-30 15:46:17 -0400

    still in progress

--- src/pages/programs.tsx
+++ src/pages/programs.tsx
@@ -8,7 +8,7 @@ export default function Programs() {
       // {index 1} index in mapping
       title: "Init Explore",
       image:
-        "https://s3-alpha-sig.figma.com/img/bdfa/e996/ec49d92b4a38e9615eb9f39c6126b252?Expires=1699228800&Signature=dftADo4bJNSyWDw8ZDgxKAgQmRS86965Ckp3piZEAAQTQdTiqizRrCtSIyOeCuL1jszTfA9qYNxyfc2mPZ2aCOtDVSSTid13ov6DCQu-x4EwSFmpkGucnkvdLOJM8cgpzHul3Mf4cLRGU2LWLfdmWGg6DyVG4e1YZUaBtfz3fsAtI4ocjbeHyVHs43gm4y61DiX~79T7iQpnSY~KZ0lBLQrRhqMqHJ13DibK-JEAIbRrUm2vVS~ljaVTQIUZ06T~-PJBQWrcm4E4yqc6RV-MPXXs6BTSguZRjhTW-eZ9Tz9NJnhX2ny89QA8HMPnnQ6yTKdlksoSFl6jRkaV5sQobw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+        "https://s3-alpha-sig.figma.com/img/6681/fe34/73232e8ff3804ccb0937eb3944ba4bbe?Expires=1699228800&Signature=X9MgB4~CaNI6e-qY7JWPah-4KmmJ~qHKlScfZMRlOoSAX25hjKcu9Mvh0BfzyU7n9tLN-Po~h00RiOOeFHrKzmWkt2-~-hLrTeJID5yosJw36tejowusqWHverWtchi0IG5eZU8P4tJxI5AK0lpfU1D228ibHL3rf3L6XXQKy2yFU3-gmsOvCxk6XvF~txZRHBDGnjbiFu67ltQFdtZYE81ByCcKaqpZd8m0hrZk0G0hZuJfBRUI0CSjC2pDk4s~~cs8iny-5iYGqf5hCpGw50b-B4msEKLB1Smo~lRlayDO-ujiT4YnGdLkH0XnWSOlQs6orreihxlh4Z4kjZiGNw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
       discription:
         "INIT Explore is a program focused on the software development side of technology. From web development to machine learning to simple scripts, this program aims to expose members to all sorts of different programming languages and frameworks commonly used in the industry.",
 
@@ -20,7 +20,7 @@ export default function Programs() {
       // {index 2} index in mapping
       title: "init reach",
       image:
-        "https://s3-alpha-sig.figma.com/img/0ec1/da54/6cf38b347bee1dcc6b49c386b1768072?Expires=1698624000&Signature=ku4MuOGJU-HqRhaLZG~rQ~ZMWFYb8LiSZT42juYGag3E0Piyh1oc9YyjR2dOr6HDbqd5iLrGZtTPfLLwZdRWlI~moY2YC0n2pD4QrjtWYjp0osOQtFTgKuVMNFSoxNNMXYnb-VDB05vsZLfVZDrrRAm1LPst5suNPFOnJCJQiZ1JXVVCEpAhjyYUTrQ8X~Lb19evV0zTyNZbHnJZBxwyEgwEWevONS13a8A9qV-g9sj3j9NIzJKVjeiuOIhVCvncvaeJTZstOBnCFmcT14o9jeO7eX9m1FX1SJ1i28YN8geAHACH6yS8mKLwQbMJS1OPjpRb2bTZGtPXO5z5NiD9aw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+        "https://s3-alpha-sig.figma.com/img/82aa/b3c6/78ea0ccf36fe437a336d5bb6acd9ac90?Expires=1699228800&Signature=YEYyd~ngtSMsTuD3mvQmZz80GvgxRAj5ML7blZ4GBunpWyjFFa8IbdA6d7jorksb9WPcUmIvOhelcpc4DROXU-3uLRnNetPqnpOO4j~Jl3Ax1PEnh7HVukR3ZfVXCFWhj~IHOtoLiFzE1D6zNdPt9IHw9z64~qGF--YX-7af0GmO6A8~VfcNhbnnmvObXfKzhRXvf5UW0rAaoPoFw5joMd3zm3IspZ6o5F~U1ePukUoCvMndx~rjT5uugnbYLh~FsjZ-FPhynZLAiE2EtPyFfDtSz75ms2iPsLiXw6Wh1Py92R1xuIqGGrkh7ZvyzejHifcZH32avSTokVxyTGbMtg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
       discription:
         "INIT Reach is a program focused on the hardware development side of technology. Using all sorts of machinery, large or small, this program aims inspire members to create their own devices with the technology available today.",
       open: true,
@@ -28,7 +28,7 @@ export default function Programs() {
     },
     {
       // {index 3} index in mapping
-      title: "init explore",
+      title: "Init Build",
       image:
         "https://s3-alpha-sig.figma.com/img/1768/8492/709c016b0d1b66b4d4022e8e00245f52?Expires=1699228800&Signature=GWQs6EoiUUDSDKKyA5TvOv67JQJVA-hz4Blb3oVRISYz-mYGUtgAn56nuhfq1KErhZncwX1rQ1xtXcWqaiV8PkL3OYtIxYvZ5MKtnBSa~9mJz23uNiU2xMpKdpyqJceInhJ~HKdIWDNriyoqh0U72zF3mb5Tpk4qOOVT5Vfv3ekl3GKuj72kfwapWp-F6ApHb38SaOpyiTHVyB7vXpkgpiIhCwla-MvOl8TRbS6kbh7vhd9sccye9v6rR~BVa8HlBaBaXaJu230URk42tf69t~Q~qjxSJXZrkwD~-1cILwZlBJrR3248gi6NgqAH7qN0-~X2I3z7Vz9PfsiyIb45Zg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
       discription:
@@ -37,50 +37,50 @@ export default function Programs() {
       url: "https://generalassemb.ly/education/user-experience-design-immersive",
     },
     {
-      // {index 3} index in mapping
-      title: "init explore",
+      // {index 4} index in mapping
+      title: "Init Ignite",
       image:
-        "https://s3-alpha-sig.figma.com/img/1768/8492/709c016b0d1b66b4d4022e8e00245f52?Expires=1699228800&Signature=GWQs6EoiUUDSDKKyA5TvOv67JQJVA-hz4Blb3oVRISYz-mYGUtgAn56nuhfq1KErhZncwX1rQ1xtXcWqaiV8PkL3OYtIxYvZ5MKtnBSa~9mJz23uNiU2xMpKdpyqJceInhJ~HKdIWDNriyoqh0U72zF3mb5Tpk4qOOVT5Vfv3ekl3GKuj72kfwapWp-F6ApHb38SaOpyiTHVyB7vXpkgpiIhCwla-MvOl8TRbS6kbh7vhd9sccye9v6rR~BVa8HlBaBaXaJu230URk42tf69t~Q~qjxSJXZrkwD~-1cILwZlBJrR3248gi6NgqAH7qN0-~X2I3z7Vz9PfsiyIb45Zg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+        "https://s3-alpha-sig.figma.com/img/e493/479b/710e8134263af482bdfcbcd1106330bd?Expires=1699228800&Signature=OBacKnEMg5fAD~Sx3lb2e0USE9ZQLfGEPwwy0sfVQeEls1AOWAcncEBk9BRv3yT0DuXZfGfqMnh7RUTuHondzmVIYVswsU4zgJorkZ25pQ5U3EjaAuWZTos4NBg53T2PdT6jMBy2a9~RKHlvyIFV3cEgBQj7l1RJziPxeQCHAqLaGYqyt1PJmGGnej6YgwbayxzrxnZoUufcKsTivSyAxsAJDMrSEoiswycMYvFpCkllslPzDrbYrUx2LVPCby12FMLrmd5qBV7bVc8KRxkljdR-2xHwsAtiH51Zep7ENRV5OgsJrRfef3GmKBZasW~6doMGkPxZ8imsGP-uZk3ZbQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      discription:
+        "INIT Ignite is a program focused on information technology, a field in technology. Commonly overlooked, information technology ensures that the systems running the software are secured and works. ",
+      open: false,
+      url: "https://generalassemb.ly/education/user-experience-design-immersive",
+    },
+    {
+      // {index 5} index in mapping
+      title: "init Hack",
+      image:
+        "https://s3-alpha-sig.figma.com/img/96ab/1321/85913dfeb40c26affd8b72e08ef7bfdb?Expires=1699228800&Signature=jFyzXu5AY7MfiZBWdWk2PwRlkFlggVl0ZpO4Jfmbmu~GE1SFDM0U4tcrIBVk9cfrb7A0nKIuOb2mvqENKloggJhrcpaF4dLa0lMGZ64hKQhyjmIG6UzdEFlq45dRz8nZ3MOctrrZgPXO0~E8-pybR-SusOV-cXCzRwccfH6nMw1iu5JU7JO39Aw9cPLRdPM9lu1jGx86sPM5WeaK6Rriw5X~x94UNxAeyJUub2BwpkXKVMKJs6uezEV728D8Q8onaMDDpVXIKG63ogDzN-2qlHCwYseP84HGufkA0IleZL0OFbjbtf-OaCCZZmDyGZC0~2s3OCIQqtuxnhzBAA5BVQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      discription:
+        "INIT Hack is a program focused on the community in Computer Science. Across the country, there are various coding competitions, Hackathons, where students must create a solution by the end of a given time frame. Most students work as teams, with the members being fellow students in the same school. ",
+      open: false,
+      url: "https://generalassemb.ly/education/user-experience-design-immersive",
+    },
+    {
+      // {index 6} index in mapping
+      title: "Init Discover",
+      image:
+        "https://s3-alpha-sig.figma.com/img/8f6b/77fb/981a2e6ce87c7537a1fd4017152342d5?Expires=1699228800&Signature=PE0kazZFlLKohUdp8SlfCyVS31z1KP8JzGf85Cn8UCvWr7pEn5K5nkas9iUmT7A63jwAwc~lKMVKFDdOaj3lrPWYjEsTSYlls3Huzkt9pS22hFHfbcoLzHo0-irWkz-8mur4KIQf3TV1GSNec0BupPmOYSiU7XJmybgd7GL5S9nLmXhb2064vtwyoi-WeFWj~UOTKx07GQ1UwyRsC79hLggjJtOPikYZPs1F1Zw6pTjyoA86XIixf08xPDZjNhuNU3E5QoXHvs11VvF1umLgmRqfKrTCtKLwYf8q52JTEZOydgf~79KPf4iiL3rYTxeUL3ckDGYaN9cr-kzm0Tx7bw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      discription:
+        "INIT Discover is a program focused on the academic side of technology. Though often overlooked, research is a viable career path for students interested in academics or furthering advancements in the field. ",
+      open: false,
+      url: "https://generalassemb.ly/education/user-experience-design-immersive",
+    },
+    {
+      // {index 7} index in mapping
+      title: "",
+      image:
+        "https://s3-alpha-sig.figma.com/img/bdfa/e996/ec49d92b4a38e9615eb9f39c6126b252?Expires=1699228800&Signature=dftADo4bJNSyWDw8ZDgxKAgQmRS86965Ckp3piZEAAQTQdTiqizRrCtSIyOeCuL1jszTfA9qYNxyfc2mPZ2aCOtDVSSTid13ov6DCQu-x4EwSFmpkGucnkvdLOJM8cgpzHul3Mf4cLRGU2LWLfdmWGg6DyVG4e1YZUaBtfz3fsAtI4ocjbeHyVHs43gm4y61DiX~79T7iQpnSY~KZ0lBLQrRhqMqHJ13DibK-JEAIbRrUm2vVS~ljaVTQIUZ06T~-PJBQWrcm4E4yqc6RV-MPXXs6BTSguZRjhTW-eZ9Tz9NJnhX2ny89QA8HMPnnQ6yTKdlksoSFl6jRkaV5sQobw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
       discription:
         "INIT Build is a program focused on project development. Teams have nine weeks to complete a project corresponding to a topic in technology that they may be interested in. From mobile applications to games to even robots.",
       open: false,
       url: "https://generalassemb.ly/education/user-experience-design-immersive",
     },
     {
-      // {index 3} index in mapping
-      title: "init explore",
+      // {index 8} index in mapping
+      title: "",
       image:
-        "https://s3-alpha-sig.figma.com/img/1768/8492/709c016b0d1b66b4d4022e8e00245f52?Expires=1699228800&Signature=GWQs6EoiUUDSDKKyA5TvOv67JQJVA-hz4Blb3oVRISYz-mYGUtgAn56nuhfq1KErhZncwX1rQ1xtXcWqaiV8PkL3OYtIxYvZ5MKtnBSa~9mJz23uNiU2xMpKdpyqJceInhJ~HKdIWDNriyoqh0U72zF3mb5Tpk4qOOVT5Vfv3ekl3GKuj72kfwapWp-F6ApHb38SaOpyiTHVyB7vXpkgpiIhCwla-MvOl8TRbS6kbh7vhd9sccye9v6rR~BVa8HlBaBaXaJu230URk42tf69t~Q~qjxSJXZrkwD~-1cILwZlBJrR3248gi6NgqAH7qN0-~X2I3z7Vz9PfsiyIb45Zg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
-      discription:
-        "INIT Build is a program focused on project development. Teams have nine weeks to complete a project corresponding to a topic in technology that they may be interested in. From mobile applications to games to even robots.",
-      open: false,
-      url: "https://generalassemb.ly/education/user-experience-design-immersive",
-    },
-    {
-      // {index 3} index in mapping
-      title: "init explore",
-      image:
-        "https://s3-alpha-sig.figma.com/img/1768/8492/709c016b0d1b66b4d4022e8e00245f52?Expires=1699228800&Signature=GWQs6EoiUUDSDKKyA5TvOv67JQJVA-hz4Blb3oVRISYz-mYGUtgAn56nuhfq1KErhZncwX1rQ1xtXcWqaiV8PkL3OYtIxYvZ5MKtnBSa~9mJz23uNiU2xMpKdpyqJceInhJ~HKdIWDNriyoqh0U72zF3mb5Tpk4qOOVT5Vfv3ekl3GKuj72kfwapWp-F6ApHb38SaOpyiTHVyB7vXpkgpiIhCwla-MvOl8TRbS6kbh7vhd9sccye9v6rR~BVa8HlBaBaXaJu230URk42tf69t~Q~qjxSJXZrkwD~-1cILwZlBJrR3248gi6NgqAH7qN0-~X2I3z7Vz9PfsiyIb45Zg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
-      discription:
-        "INIT Build is a program focused on project development. Teams have nine weeks to complete a project corresponding to a topic in technology that they may be interested in. From mobile applications to games to even robots.",
-      open: false,
-      url: "https://generalassemb.ly/education/user-experience-design-immersive",
-    },
-    {
-      // {index 3} index in mapping
-      title: "init explore",
-      image:
-        "https://s3-alpha-sig.figma.com/img/1768/8492/709c016b0d1b66b4d4022e8e00245f52?Expires=1699228800&Signature=GWQs6EoiUUDSDKKyA5TvOv67JQJVA-hz4Blb3oVRISYz-mYGUtgAn56nuhfq1KErhZncwX1rQ1xtXcWqaiV8PkL3OYtIxYvZ5MKtnBSa~9mJz23uNiU2xMpKdpyqJceInhJ~HKdIWDNriyoqh0U72zF3mb5Tpk4qOOVT5Vfv3ekl3GKuj72kfwapWp-F6ApHb38SaOpyiTHVyB7vXpkgpiIhCwla-MvOl8TRbS6kbh7vhd9sccye9v6rR~BVa8HlBaBaXaJu230URk42tf69t~Q~qjxSJXZrkwD~-1cILwZlBJrR3248gi6NgqAH7qN0-~X2I3z7Vz9PfsiyIb45Zg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
-      discription:
-        "INIT Build is a program focused on project development. Teams have nine weeks to complete a project corresponding to a topic in technology that they may be interested in. From mobile applications to games to even robots.",
-      open: false,
-      url: "https://generalassemb.ly/education/user-experience-design-immersive",
-    },
-    {
-      // {index 3} index in mapping
-      title: "init explore",
-      image:
-        "https://s3-alpha-sig.figma.com/img/1768/8492/709c016b0d1b66b4d4022e8e00245f52?Expires=1699228800&Signature=GWQs6EoiUUDSDKKyA5TvOv67JQJVA-hz4Blb3oVRISYz-mYGUtgAn56nuhfq1KErhZncwX1rQ1xtXcWqaiV8PkL3OYtIxYvZ5MKtnBSa~9mJz23uNiU2xMpKdpyqJceInhJ~HKdIWDNriyoqh0U72zF3mb5Tpk4qOOVT5Vfv3ekl3GKuj72kfwapWp-F6ApHb38SaOpyiTHVyB7vXpkgpiIhCwla-MvOl8TRbS6kbh7vhd9sccye9v6rR~BVa8HlBaBaXaJu230URk42tf69t~Q~qjxSJXZrkwD~-1cILwZlBJrR3248gi6NgqAH7qN0-~X2I3z7Vz9PfsiyIb45Zg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+        "https://s3-alpha-sig.figma.com/img/0ec1/da54/6cf38b347bee1dcc6b49c386b1768072?Expires=1698624000&Signature=ku4MuOGJU-HqRhaLZG~rQ~ZMWFYb8LiSZT42juYGag3E0Piyh1oc9YyjR2dOr6HDbqd5iLrGZtTPfLLwZdRWlI~moY2YC0n2pD4QrjtWYjp0osOQtFTgKuVMNFSoxNNMXYnb-VDB05vsZLfVZDrrRAm1LPst5suNPFOnJCJQiZ1JXVVCEpAhjyYUTrQ8X~Lb19evV0zTyNZbHnJZBxwyEgwEWevONS13a8A9qV-g9sj3j9NIzJKVjeiuOIhVCvncvaeJTZstOBnCFmcT14o9jeO7eX9m1FX1SJ1i28YN8geAHACH6yS8mKLwQbMJS1OPjpRb2bTZGtPXO5z5NiD9aw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
       discription:
         "INIT Build is a program focused on project development. Teams have nine weeks to complete a project corresponding to a topic in technology that they may be interested in. From mobile applications to games to even robots.",
       open: false,
@@ -89,16 +89,20 @@ export default function Programs() {
   ];
   return (
     <main className="min-h-screen">
-      <div className="flex items-center justify-between">
-        <h1 className="mt-6 text-3xl text-white">Programs</h1>
-        <div className="flex items-center space-x-5"></div>
+      <div className="mb-4 flex flex-col items-center justify-between md:mb-0 md:flex-row">
+        <h1 className="mb-4 text-2xl text-white md:mb-0 md:text-3xl">
+          Programs
+        </h1>
       </div>
-      <div className="Cards grid grid-cols-4 gap-x-8 gap-y-4 space-x-6">
+      <div className=" grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {/* what ever comes first in the array thats the objects in the array and whats second is the Index */}
         {sampleData.map((programData, index) => {
           return (
-            <div key={index} className="mt-12 flex flex-row  space-x-10">
-              <div className="dark:border-none-600  w-full max-w-sm rounded-lg border border-gray-200 bg-white shadow dark:bg-gray-800  ">
+            <div key={index} className="mx-auto mt-4 ">
+              <div
+                className=" w-full max-w-sm  bg-white shadow dark:bg-gray-900"
+                style={{ height: "100%" }}
+              >
                 <a href="#">
                   <img
                     className="rounded-t-lg"
@@ -106,13 +110,13 @@ export default function Programs() {
                     src={programData.image}
                   />
                 </a>
-                <div className="p-5">
+                <div className="p-5" style={{ height: "100%" }}>
                   <a href="#">
                     <h5 className="light:text-red mb-2 text-2xl font-bold tracking-tight text-red-500">
                       {programData.title}
                     </h5>
                   </a>
-                  <p className="text-none-700 dark:text-none-400 mb-3 font-poppins">
+                  <p className="text-none-500 dark:text-none-500 mb-6 font-poppins">
                     {programData.discription}
                   </p>
                   {/* (Ternary.Operator) anything before ? is the condition if the conditon is true it
