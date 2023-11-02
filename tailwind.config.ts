caa7e8cf54f87c0d1eece685d61aed52a5e62b9e
David Barcenas <dabarcenas6921@gmail.com>
2023-11-02 16:06:05 -0400

    added fonts folder

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,10 +24,11 @@ export default {
         "160": "40rem", // Adds h-160 for 40rem/640px height
       },
       fontFamily: {
-        bulky: ["PolysansBulky", "sans-serif"],
-        neutralMono: ["PolysansNeutralMono", "sans-serif"],
-        neutral: ["PolysansNeutral", "sans-serif"],
-        median: ["PolysansMedian", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        bulky: ["PolysansBulky", "Poppins", "sans-serif"],
+        neutralMono: ["PolysansNeutralMono", "Poppins", "sans-serif"],
+        neutral: ["PolysansNeutral", "Poppins", "sans-serif"],
+        median: ["PolysansMedian", "Poppins", "sans-serif"],
       },
     },
   },
