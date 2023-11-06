6b8bf309d6ff2cff25d5900a37931c07c6b583b4
Mateo Carrion <crip_bippas@mateos-mbp.attlocal.net>
2023-11-06 15:38:48 -0500

    Completed sign-in stylings

--- src/pages/sign-in/[[...index]].tsx
+++ src/pages/sign-in/[[...index]].tsx
@@ -2,26 +2,43 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Login() {
   return (
-    <div className="border-radius pb-43 flex justify-center">
+    <div className="flex min-h-screen justify-center">
       <SignIn
         appearance={{
           elements: {
             formFieldLabel: {
               fontSize: 18,
             },
+            formFieldInput: {
+              color: "black",
+            },
+            formFieldInputShowPasswordButton: {
+              backgroundColor: "#D1D1D1",
+            },
+            formFieldInputGroup: {},
+            identityPreviewEditButton: {
+              color: "#D1D1D1",
+              "&:hover, &:focus, &:active": {
+                backgroundColor: "#D1D1D1",
+              },
+            },
+            card: {
+              borderRadius: 0,
+            },
             formButtonPrimary: {
               color: "#19191A",
               fontSize: 18,
               textTransform: "none",
+              borderRadius: 0,
               backgroundColor: "#FFD550",
               "&:hover, &:focus, &:active": {
-                backgroundColor: "#DAAD06",
+                backgroundColor: "#FFF1B8",
               },
               footerActionText: {
                 backgroundColor: "#FFD550",
               },
             },
-            logoImage: {
+            logoBox: {
               filter: "brightness(1)",
             },
             b3fm6y: {
