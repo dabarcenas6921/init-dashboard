0815c391b01a6745f144ce05c6fbd78cb45c0aae
Zeshan Khatri <zeshan.khatri@gmail.com>
2023-11-09 16:58:17 -0500

    Seed command works now.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,19 +1,21 @@
-import { jobPostings } from '../src/Data/jobPostingsData'
-import { PrismaClient } from '@prisma/client'
+import { jobPostings } from "../src/Data/jobPostingsData";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 async function main() {
-    for (let jobPosting of jobPostings) {
-        await prisma.jobPosting.create({
-            data: jobPosting
-        })
-    }
+  for (const jobPosting of jobPostings) {
+    await prisma.jobPosting.create({
+      data: jobPosting,
+    });
+  }
 }
 
-main().catch(e => {
+main()
+  .catch((e) => {
     console.log(e);
-    process.exit(1)
-}).finally(() => {
-    prisma.$disconnect();
-})
+    process.exit(1);
+  })
+  .finally(() => {
+    void prisma.$disconnect();
+  });
