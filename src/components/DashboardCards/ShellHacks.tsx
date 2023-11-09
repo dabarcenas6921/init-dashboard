1d6a526751ff7c37fa66887fbe06e5c3639c38bf
David Barcenas <dabarcenas6921@gmail.com>
2023-11-09 17:35:40 -0500

    calling airtable api

--- src/components/DashboardCards/ShellHacks.tsx
+++ src/components/DashboardCards/ShellHacks.tsx
@@ -2,13 +2,10 @@
 import React, { Component } from "react";
 import MasonBody from "./Mason";
 
-export default class ShellHacks extends Component {
-  render() {
-    console.log("Component is rendering");
-    return (
-      <div className="">
-        <MasonBody />
-      </div>
-    );
-  }
+export default function ShellHacks() {
+  return (
+    <div className="">
+      <MasonBody />
+    </div>
+  );
 }
