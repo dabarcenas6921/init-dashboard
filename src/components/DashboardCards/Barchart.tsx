fedde63e5c948b6c007efd1c2d04c9a385b837b3
AngeloBoggio <91862454+AngeloBoggio@users.noreply.github.com>
2023-11-09 17:10:39 -0500

    Finished Shellhacks dashboard

--- src/components/DashboardCards/Barchart.tsx
+++ src/components/DashboardCards/Barchart.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import {
   BarChart,
   Bar,
-  Rectangle,
   XAxis,
   YAxis,
   CartesianGrid,
@@ -27,8 +26,6 @@ export default function MyBarChart() {
       <div className="mt-5 max-w-full" style={{ width: "100%", height: 581 }}>
         <ResponsiveContainer width="100%" height="100%">
           <BarChart
-            width={400}
-            height={490}
             data={data}
             margin={{
               top: 0,
@@ -37,19 +34,27 @@ export default function MyBarChart() {
               bottom: 5,
             }}
           >
-            <CartesianGrid />
-            <XAxis dataKey="name" stroke="white" /> {/* Updated this line */}
-            <YAxis stroke="white" /> {/* Updated this line */}
-            <Tooltip />
-            <Bar
-              dataKey="$"
-              fill="#FFD550"
-              activeBar={<Rectangle fill="pink" stroke="blue" />}
+            <defs>
+              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#FFD550" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#FFD550" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+
+            <XAxis dataKey="name" stroke="white" />
+            <YAxis stroke="white" />
+            <Tooltip
+              contentStyle={{ backgroundColor: "white", color: "black" }}
+              formatter={(value) => [`$${value.toLocaleString()}`]}
+              labelFormatter={() => ""} // Add this line to hide the label
             />
+            <Bar dataKey="$" fill="url(#colorUv)" />
           </BarChart>
         </ResponsiveContainer>
       </div>
-      <p className="flex justify-center text-xl">Total: ${totalValue}</p>
+      <p className="flex justify-center text-xl">
+        Total: ${totalValue.toLocaleString()}
+      </p>
     </div>
   );
 }
--- src/components/DashboardCards/Mason.tsx
+++ src/components/DashboardCards/Mason.tsx
@@ -7,10 +7,12 @@ import Piechart from "./Piechart";
 
 export default function MasonBody() {
   return (
+    // On mobile, the content will stack (flex-col). On medium screens and up, it will be side by side (md:flex-row).
     <div className="flex flex-col md:flex-row">
-      {/* Adjusted the flex value of this div to flex-1 */}
-      <div className="mr-5 flex flex-1 flex-col">
-        <div className="mb-5 flex flex-col justify-start space-y-5 md:flex-row md:space-x-5 md:space-y-0">
+      {/* This div will take full width on mobile and flex-1 (50% of the parent container) on medium screens and up */}
+      <div className="flex w-full flex-col md:mr-5 md:flex-1">
+        {/* On mobile, each child will stack. On medium screens, they will be side by side */}
+        <div className="flex flex-col justify-start space-y-5 md:flex-row md:space-x-5 md:space-y-0">
           <div className="w-full p-2">
             <a
               href="#"
@@ -29,7 +31,7 @@ export default function MasonBody() {
           </div>
         </div>
         <div className="flex flex-col justify-start space-y-5 md:space-y-0">
-          <div className="mb-8 w-full p-2">
+          <div className="mb-8 mt-5 w-full p-2">
             <a
               href="#"
               className="block w-full rounded-lg bg-zinc-800 p-4 shadow-xl"
@@ -37,7 +39,6 @@ export default function MasonBody() {
               <YearToDate />
             </a>
           </div>
-          {/* Removed the flex-grow class, retained mt-10 and w-full */}
           <div className="w-full p-2">
             <a
               href="#"
@@ -48,8 +49,8 @@ export default function MasonBody() {
           </div>
         </div>
       </div>
-      {/* This div remains flex-1, ensuring equal width with the left side */}
-      <div className="flex flex-1 flex-col">
+      {/* This div will take full width on mobile and flex-1 (50% of the parent container) on medium screens and up */}
+      <div className="mt-5 flex w-full flex-col md:mt-0 md:flex-1">
         <div className="p-2">
           <a
             href="#"
