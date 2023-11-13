21e7a284b53d62d7060f86bc11d93ce5b165b316
AngeloBoggio <91862454+AngeloBoggio@users.noreply.github.com>
2023-11-13 16:57:07 -0500

    Using API & Parsing Data for the volunteer info

--- src/components/DashboardCards/TeamLeads.tsx
+++ src/components/DashboardCards/TeamLeads.tsx
@@ -1,98 +1,24 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
 const baseId = process.env.NEXT_PUBLIC_AIRTABLE_SHELLHACKS_BASE_ID;
 const tableId =
   process.env.NEXT_PUBLIC_AIRTABLE_SHELLHACKS_VOLUNTEER_SCHEDULE_TABLE;
 const apiKey = process.env.NEXT_PUBLIC_AIRTABLE_PERSONAL_ACCESS_TOKEN;
 
-const data = [
-  {
-    name: "Gerald",
-    schedule: "Fri - Sat",
-    time: "9:00 AM - 5:00 PM",
-    contact: "foolsfool@gmail.com",
-  },
-  {
-    name: "Steve",
-    schedule: "Sat - Sun",
-    time: "9:00 AM - 5:00 PM",
-    contact: "foolsfool@gmail.com",
-  },
-  {
-    name: "Sam",
-    schedule: "Mon - Fri",
-    time: "9:00 AM - 5:00 PM",
-    contact: "foolsfool@gmail.com",
-  },
-  {
-    name: "Andy",
-    schedule: "Sat - Fri",
-    time: "9:00 AM - 5:00 PM",
-    contact: "foolsfool@gmail.com",
-  },
-  {
-    name: "Kevin",
-    schedule: "Fri",
-    time: "9:00 AM - 5:00 PM",
-    contact: "foolsfool@gmail.com",
-  },
-  {
-    name: "John",
-    schedule: "Mon - Fri",
-    time: "9:00 AM - 5:00 PM",
-    contact: "foolsfool@gmail.com",
-  },
-  {
-    name: "Mike",
-    schedule: "Mon - Fri",
-    time: "9:00 AM - 5:00 PM",
-    contact: "foolsfool@gmail.com",
-  },
-  {
-    name: "Mike",
-    schedule: "Mon - Fri",
-    time: "9:00 AM - 5:00 PM",
-    contact: "foolsfool@gmail.com",
-  },
-  {
-    name: "Mike",
-    schedule: "Mon - Fri",
-    time: "9:00 AM - 5:00 PM",
-    contact: "foolsfool@gmail.com",
-  },
-  {
-    name: "Mike",
-    schedule: "Mon - Fri",
-    time: "9:00 AM - 5:00 PM",
-    contact: "foolsfool@gmail.com",
-  },
-  {
-    name: "Mike",
-    schedule: "Mon - Fri",
-    time: "9:00 AM - 5:00 PM",
-    contact: "foolsfool@gmail.com",
-  },
-  {
-    name: "Mike",
-    schedule: "Mon - Fri",
-    time: "9:00 AM - 5:00 PM",
-    contact: "foolsfool@gmail.com",
-  },
-  {
-    name: "Mike",
-    schedule: "Mon - Fri",
-    time: "9:00 AM - 5:00 PM",
-    contact: "foolsfool@gmail.com",
-  },
-  {
-    name: "Mike",
-    schedule: "Mon - Fri",
-    time: "9:00 AM - 5:00 PM",
-    contact: "foolsfool@gmail.com",
-  },
-];
+interface RecordType {
+  fields: {
+    Name: string;
+    "Last Name": string;
+    "Discord ID copy": string;
+    Times: string[];
+  };
+  // Include other properties if needed
+}
 
 export default function MyBarChart() {
+  const [volunteers, setVolunteers] = useState([]);
+
   useEffect(() => {
     const fetchDataFromAirtable = async () => {
       try {
@@ -104,9 +30,37 @@ export default function MyBarChart() {
             },
           },
         );
-        console.log(response.data);
+
+        // First, filter out any records where the necessary fields are not defined
+        const validRecords = response.data.records.filter(
+          (record) =>
+            record.fields.Name &&
+            record.fields["Last Name"] &&
+            record.fields.Times &&
+            record.fields.Times.length > 0,
+        );
+
+        // Then, map over the filtered records to transform them into the desired shape
+        const fetchedData = validRecords.map((record) => {
+          // Extract the schedule without time details
+          const schedule = record.fields.Times[0].split(":")[0].trim();
+
+          // Extract the time range
+          const timeRangeMatch = record.fields.Times[0].match(
+            /(\d{1,2}[APM\s]+-\s*\d{1,2}[APM\s]+)/,
+          );
+          const time = timeRangeMatch ? timeRangeMatch[0] : "No time available"; // Extract the time range or provide "No time available" if the format doesn't match
+
+          return {
+            name: `${record.fields.Name} ${record.fields["Last Name"]}`,
+            schedule: schedule,
+            time: time,
+            contact: record.fields["Discord ID copy"],
+          };
+        });
+
+        setVolunteers(fetchedData);
       } catch (error) {
-        // Handle error
         console.error(error);
       }
     };
@@ -120,9 +74,9 @@ export default function MyBarChart() {
         Volunteer Schedule
       </h1>
       <div className="max-h-[420px] overflow-y-auto">
-        {/* Use divs instead of table on small screens */}
+        {/* Display data in cards for smaller screens */}
         <div className="mt-5 w-full text-sm text-gray-400 sm:hidden">
-          {data.map((item, index) => (
+          {volunteers.map((item, index) => (
             <div key={index} className="mb-4 rounded bg-zinc-800 p-4">
               <div className="font-medium text-white">{item.name}</div>
               <div className="text-yellow-300">{item.schedule}</div>
@@ -131,7 +85,8 @@ export default function MyBarChart() {
             </div>
           ))}
         </div>
-        {/* Show table on larger screens */}
+
+        {/* Display data in a table for larger screens */}
         <table className="hidden w-full text-left text-gray-400 sm:block">
           <thead className="bg-zinc-800 text-xs uppercase text-gray-700">
             <tr>
@@ -150,7 +105,7 @@ export default function MyBarChart() {
             </tr>
           </thead>
           <tbody>
-            {data.map((item, index) => (
+            {volunteers.map((item, index) => (
               <tr key={index} className="border-b border-gray-700 bg-zinc-800">
                 <td className="px-6 py-4 font-medium text-white">
                   {item.name}
