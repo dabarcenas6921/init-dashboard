2225c31b1d1ed5a22613148510ea2664a8aa5a0b
David Barcenas <dabarcenas6921@gmail.com>
2023-11-13 17:31:07 -0500

    changed piechart

--- src/components/DashboardCards/Piechart.tsx
+++ src/components/DashboardCards/Piechart.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { PieChart, Pie, Tooltip, ResponsiveContainer, Cell } from "recharts";
+import {
+  PieChart,
+  Pie,
+  Tooltip,
+  ResponsiveContainer,
+  Cell,
+  Legend,
+} from "recharts";
 
 // Define an interface for your data objects
 interface IData {
@@ -8,13 +15,20 @@ interface IData {
   color: string;
 }
 
+//Ideally, you would want to fetch this data from the Notion/Airtable API. Don't have permissions to do so right now, so I'm hardcoding it in.
 const data01: IData[] = [
-  { name: "Google", value: 400, color: "#0088FE" },
-  { name: "Microsoft", value: 300, color: "#00C49F" },
-  { name: "FIU", value: 300, color: "#FFBB28" },
-  { name: "Salesforce", value: 200, color: "#FF8042" },
-  { name: "Bungie", value: 278, color: "#8884d8" },
-  { name: "Knight Foundation", value: 189, color: "#A4DE6C" },
+  { name: "Venue", value: 70792, color: "#5a2c4c" },
+  { name: "Food", value: 34200, color: "#fa14d2" },
+  { name: "Snacks and drinks", value: 8000, color: "#619203" },
+  { name: "Decorations", value: 3000, color: "#23e0fa" },
+  { name: "Shirts", value: 10256, color: "#4c6e6b" },
+  { name: "Lanyard/Badge", value: 2754.15, color: "#2061e7" },
+  { name: "Tote Bags", value: 2500, color: "#035353" },
+  { name: "Busses", value: 14392.94, color: "#32b53a" },
+  { name: "Incidentals (Fridge, Ice)", value: 1750, color: "#c24b73" },
+  { name: "Custodial Staff", value: 3700, color: "#41d16a" },
+  { name: "Extra dumpster and tonnage of trash", value: 500, color: "#53879a" },
+  { name: "AskIT - Public IP Address setup", value: 578.99, color: "#22147b" },
 ];
 
 // Calculate the total funding
@@ -32,7 +46,6 @@ export default function Piechart() {
         Company Funding
       </p>
       <div className="flex justify-center">
-        {/* Use Tailwind's responsive prefix to set different aspect ratios for different screen sizes */}
         <ResponsiveContainer
           width="100%"
           className="aspect-square sm:aspect-video"
@@ -46,14 +59,14 @@ export default function Piechart() {
               cy="50%"
               outerRadius={140}
               fill="#8884d8"
-              labelLine={false}
-              label={({ name }: LabelProps) => name}
+              // Remove labelLine and label props if not needed
             >
               {data01.map((entry, index) => (
                 <Cell key={`cell-${index}`} fill={entry.color} />
               ))}
             </Pie>
             <Tooltip />
+            <Legend layout="vertical" align="right" verticalAlign="middle" />
           </PieChart>
         </ResponsiveContainer>
       </div>
