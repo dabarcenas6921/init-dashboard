35159724ef4d49d6e4372a4085b8056657037a80
David Barcenas <dabarcenas6921@gmail.com>
2023-11-14 23:27:26 -0500

    changed values for the piechart

--- src/components/DashboardCards/ShellhacksCards/Piechart.tsx
+++ src/components/DashboardCards/ShellhacksCards/Piechart.tsx
@@ -17,18 +17,11 @@ interface IData {
 
 //Ideally, you would want to fetch this data from the Notion/Airtable API. Don't have permissions to do so right now, so I'm hardcoding it in.
 const data01: IData[] = [
-  { name: "Venue", value: 70792, color: "#5a2c4c" },
-  { name: "Food", value: 34200, color: "#fa14d2" },
-  { name: "Snacks and drinks", value: 8000, color: "#619203" },
-  { name: "Decorations", value: 3000, color: "#23e0fa" },
-  { name: "Shirts", value: 10256, color: "#4c6e6b" },
-  { name: "Lanyard/Badge", value: 2754.15, color: "#2061e7" },
-  { name: "Tote Bags", value: 2500, color: "#035353" },
-  { name: "Busses", value: 14392.94, color: "#32b53a" },
-  { name: "Incidentals (Fridge, Ice)", value: 1750, color: "#c24b73" },
-  { name: "Custodial Staff", value: 3700, color: "#41d16a" },
-  { name: "Extra dumpster and tonnage of trash", value: 500, color: "#53879a" },
-  { name: "AskIT - Public IP Address setup", value: 578.99, color: "#22147b" },
+  { name: "Venue", value: 1000, color: "#e11d48" },
+  { name: "Food", value: 2000, color: "#3b82f6" },
+  { name: "Snacks and drinks", value: 4000, color: "#16a34a" },
+  { name: "Decorations", value: 5000, color: "#fcd34d" },
+  { name: "Shirts", value: 3000, color: "#fb923c" },
 ];
 
 // Calculate the total funding
@@ -43,7 +36,7 @@ export default function Piechart() {
   return (
     <div>
       <p className="flex justify-center font-medium text-white">
-        Company Funding
+        Shellhacks Budget
       </p>
       <div className="flex justify-center">
         <ResponsiveContainer
