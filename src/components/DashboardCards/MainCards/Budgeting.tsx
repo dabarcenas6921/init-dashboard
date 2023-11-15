a312a31f552e6430c892933ee5a4e5a0bec3fea5
David Barcenas <dabarcenas6921@gmail.com>
2023-11-15 18:18:07 -0500

    line graph finished

--- src/components/DashboardCards/MainCards/Budgeting.tsx
+++ src/components/DashboardCards/MainCards/Budgeting.tsx
@@ -11,48 +11,53 @@ import {
 } from "recharts";
 
 const data = [
-  { month: "January", actual: 3800 },
-  { month: "February", actual: 4200 },
-  { month: "March", actual: 3500 },
-  { month: "April", actual: 4000 },
-  { month: "May", actual: 4500 },
-  { month: "June", actual: 3700 },
-  { month: "July", actual: 4800 },
-  { month: "August", actual: 3900 },
-  { month: "September", actual: 4100 },
-  { month: "October", actual: 3600 },
-  { month: "November", actual: 4200 },
-  { month: "December", actual: 3800 },
+  { month: "January", spending: 3800 },
+  { month: "February", spending: 4200 },
+  { month: "March", spending: 3500 },
+  { month: "April", spending: 4000 },
+  { month: "May", spending: 4500 },
+  { month: "June", spending: 3700 },
+  { month: "July", spending: 4800 },
+  { month: "August", spending: 3900 },
+  { month: "September", spending: 4100 },
+  { month: "October", spending: 3600 },
+  { month: "November", spending: 4200 },
+  // ... more months
 ];
 
-const budgetValue = 4000; // Static budget value
+const budgetValue = 4000;
 
-const Budgeting: React.FC = () => {
+export default function Budgeting() {
   return (
-    <ResponsiveContainer width="100%" height="100%">
-      <LineChart
-        data={data}
-        margin={{
-          top: 10,
-          right: 30,
-          left: 0,
-          bottom: 0,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="month" />
-        <YAxis domain={["auto", "auto"]} />
-        <Tooltip />
-        <Line type="monotone" dataKey="spending" />
-        <ReferenceLine
-          y={budgetValue}
-          label="Budget"
-          stroke="red"
-          strokeDasharray="3 3"
-        />
-      </LineChart>
-    </ResponsiveContainer>
+    <div className="mx-auto max-w-4xl rounded-lg  p-4 shadow-md">
+      <h2 className="mb-4 text-center text-xl font-bold text-white">
+        Monthly Budgeting
+      </h2>
+      <div style={{ width: "100%", height: 400 }}>
+        <ResponsiveContainer>
+          <LineChart
+            data={data}
+            margin={{ top: 10, right: 30, left: 0, bottom: 20 }} // Increased bottom margin
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis
+              dataKey="month"
+              tick={{ fontSize: 10, fill: "white" }} // Smaller font size for X-axis labels
+              angle={-45} // Optional: tilt labels if needed
+              textAnchor="end"
+            />
+            <YAxis domain={[3400, 4900]} />
+            <Tooltip />
+            <Line type="monotone" dataKey="spending" stroke="#FFA500" />
+            <ReferenceLine
+              y={budgetValue}
+              label="Budget"
+              stroke="red"
+              strokeDasharray="3 3"
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
   );
-};
-
-export default Budgeting;
+}
